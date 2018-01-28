ba4ad28c662d7d9e3fed0839d48c52f37fdb7ee6
Robert Hubbard <rob.c.hubbard@gmail.com>
2018-01-28 22:46:47 +0000

    Extend documentation.

--- items.py
+++ items.py
@@ -2,10 +2,13 @@
   Counts the instances of each item in a list using `Counter`.
 
   @since 1.0.0
-  @category - Parsing
+  @category: Parsing
   @param (list) a_list: The list to count.
-  (int) [optional] n=1000: Integer to limit data returned. Default is 1000.
-  (boolean) [optional] x=False: Boolean to return list (default) or dictionary.
+  @param (boolean) [optional] dict=False: Boolean to return
+      list (default) or dictionary. Must be passed if an int is passed as
+      third param.
+  @param (int) [optional] n=1000: Integer to limit data returned.
+      Default is 1000.
   @returns (list) [default]: Returns a list.
     Each item in the list is `[occurrence total: 'value']`.
     (Dictionary) [optional]: Returns a dictionary.
@@ -15,8 +18,8 @@
   data = [a, v, a, f, d, b, d, f, d, b, d, a, f, a, a]
   results = items(data)
   limit_results = items(data, 3)
-  limit_results = items(data, 3, True)
-  
+  limit_results = items(data, True, 3)
+
   print(results)
   print(limit_results)
   // ["5: 'a'","4: 'd'", "3: 'f'", "2: 'b'",  "1: 'v'"]
@@ -29,15 +32,19 @@
 from collections import Counter
 
 
-def items(a_list, n=1000, x=False):
+def items(a_list, dict=False, n=1000):
+    # Count occurenences in the list.
     foo = Counter(a_list)
-    if x is False:
+    # Check if default list should be returned.
+    if dict is False:
         new_list = []
         for value, count in foo.most_common(n):
             new_list.append("%s: %r" % (count, value))
         return new_list
+    # Return dictionary instead of list.
     else:
         new_dict = {}
         for value, count in foo.most_common(n):
+            # Make total count the key, which is the reverse of Counter.
             new_dict[count] = value
         return new_dict
